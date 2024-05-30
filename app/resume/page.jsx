5883bb40b544062231bad70c05eb18256ece9ae9
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-05-30 15:04:10 +0200

    Add resume data

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,26 @@
+'use client';
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaNodeJs,
+  FaReact,
+  FaFigma,
+} from 'react-icons/fa';
+import { RiNextjsFill } from 'react-icons/ri';
+import {
+  SiRuby,
+  SiRubyonrails,
+  SiExpress,
+  SiMongodb,
+  SiPostgresql,
+  SiTailwindcss,
+} from 'react-icons/si';
+
+// lib
+import { about } from '@/lib/about';
+
 const Resume = () => {
   return <div>Resume Page</div>;
 };
